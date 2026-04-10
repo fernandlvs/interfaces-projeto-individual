7efdf9fb1fb7d8e8537234384b5e7ba5e448b6a4
Fernanda Alves <168235627+fernandlvs@users.noreply.github.com>
2026-04-10 18:57:13 -0300

    Update Login.jsx

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,6 +1,6 @@
 import "./Login.css";
 import { useState } from "react";
-import axios from "axios";
+import api from "../services/api";
 import { useNavigate } from "react-router-dom";
 import { FiEye, FiEyeOff } from "react-icons/fi";
 
@@ -18,8 +18,8 @@ export default function Login() {
         try {
             setLoading(true);
 
-            const response = await axios.post(
-                "http://localhost:3000/auth/login",
+            const response = await api.post(
+                    "/auth/login",
                 {
                     email,
                     senha,
